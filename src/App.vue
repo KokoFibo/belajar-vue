9770284033997bc5eb0e00eb8355fadda9c5ee1d
KokoFibo <kokonacci@gmail.com>
2025-12-25 17:57:55 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Like from "./components/Like.vue";
+
+function rubahWarna() {
+  document.querySelector("h1").style.color = "red";
+}
+</script>
+
+<template>
+  <Like />
+  <h1>You did it!</h1>
+  <button @click="rubahWarna()">Rubah Warna H1</button>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Like.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+function addLike() {
+  like.value++;
+}
+
+const like = ref(0);
+</script>
+
+<template>
+  <h3>Like({{ like }})</h3>
+  <button @click="addLike()">Like</button>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
